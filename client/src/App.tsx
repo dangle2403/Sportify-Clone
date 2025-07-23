dd9e043aa836f28c1cf628b196e1dac476f4db3f
Dang Le <dang.le.tech2403@gmail.com>
2025-07-23 15:33:15 -0500

    Implement checking for auth everytime the page reload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,18 @@
-import { Button } from "./components/ui/button"
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
-const App = () => {
-  return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-  )
-}
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage.tsx";
+import AuthCallbackPage from "./pages/authCallback/AuthCallbackPage.tsx";
 
-export default App
+const App = () => {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/pages/authCallback/AuthCallbackPage.tsx
@@ -0,0 +1,11 @@
+
+const AuthCallbackPage = () => {
+  return (
+    <div>
+
+    AuthCallbackPage
+    </div>
+  )
+}
+
+export default AuthCallbackPage
