6f1b08e28cf48164c5dc142fa1a999f4650af6d2
Dang Le <dang.le.tech2403@gmail.com>
2025-07-23 09:39:47 -0500

    Complete frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+const App = () => {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
+}
+
+export default App
