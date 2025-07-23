defe737aad69e6434a1758c8ce29df699bc62038
Dang Le <dang.le.tech2403@gmail.com>
2025-07-23 13:51:16 -0500

    Create router and controller to get all albums or get album by id

---
+++ server/src/controllers/album.controller.js
@@ -0,0 +1,27 @@
+import Album from "../models/album.model";
+
+export const getAllAlbums = async (req, res, next) => {
+  try{
+    // Fetch all albums and populate the songs field
+    const albums = await Album.find().populate("songs");
+    res.status(200).json(albums);
+  } catch (error) {
+    console.error("Error fetching albums:", error);
+    next(error);
+  }
+};
+
+export const getAlbumById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // Find album by ID and populate the songs field
+    const album = await Album.findById(id).populate("songs");
+    if (!album) {
+      return res.status(404).json({ message: "Album not found" });
+    }
+    res.status(200).json(album);
+  } catch(error) {
+    console.error("Error fetching album by ID:", error);
+    next(error);
+  }
+};
--- server/src/routes/album.route.js
+++ server/src/routes/album.route.js
@@ -1,7 +1,12 @@
-import { Router } from 'express';
-
+import { Router } from "express";
+import { protectRoute } from "../middlewares/auth.middleware";
+import { getAlbumById, getAllAlbums } from "../controllers/album.controller";
 
 const albumRouter = Router();
 
+albumRouter.use(protectRoute);
+
+albumRouter.get("/", getAllAlbums);
+albumRouter.get("/:id", getAlbumById);
 
 export default albumRouter;
